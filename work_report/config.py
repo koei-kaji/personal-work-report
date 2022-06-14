14a368717a08af669e7fc94ed8e4bac3f75dabbf
koei-kaji <koei.kaji@gmail.com>
2022-06-14 23:20:37 +0900

    feat: add config

---
+++ work_report/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from pydantic import BaseSettings
+
+
+class DatabaseSettings(BaseSettings):
+    # NOTE: https://docs.ponyorm.org/api_reference.html?highlight=create_db#supported-databases
+    provider: str = "sqlite"
+    filename: str = str(Path(__file__).parent / "sqlite.db")
+    create_db: bool = True
+    create_tables: bool = True
+
+    def dict_bind(self) -> Dict[str, Any]:
+        return self.dict(include={"provider", "filename", "create_db"})
